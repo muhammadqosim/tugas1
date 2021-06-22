44072ae472fd5515822ff589b060ca9b847c85f6
muhammadqosim <qosimmuhammad1961@gmail.com>
2021-06-22 19:24:04 +0800

    membuat Getter Setter diClass1(Seluruhnya)

--- src/model/cemilan.java
+++ src/model/cemilan.java
@@ -68,5 +68,70 @@ public class cemilan {
         this.jenis = jenis;
     }
 
+    //Getter And Setter
 
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getBuatan() {
+        return buatan;
+    }
+
+    public void setBuatan(String buatan) {
+        this.buatan = buatan;
+    }
+
+    public String getAsal() {
+        return asal;
+    }
+
+    public void setAsal(String asal) {
+        this.asal = asal;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getTelpon() {
+        return telpon;
+    }
+
+    public void setTelpon(String telpon) {
+        this.telpon = telpon;
+    }
 }
